31338f336359177a22141d9fcd94bb2a3e6cdd8a
sovushka0290 <riskulovakorpus@gmail.com>
2026-03-31 09:28:20 +0500

    Update models.py

--- minecraft_bot/database/models.py
+++ minecraft_bot/database/models.py
@@ -1,6 +1,11 @@
 import aiosqlite
 from config import DB_PATH
 
+import asyncpg
+from config import DATABASE_URL
+
+async def get_pool():
+    return await asyncpg.create_pool(DATABASE_URL)
 
 async def init_db():
     async with aiosqlite.connect(DB_PATH) as db:
